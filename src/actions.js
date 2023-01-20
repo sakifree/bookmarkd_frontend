a031ca74bcba82d3d88ce0e4ceac82de4c8afc98
Naveen Muralidharan <naveenspec@gmail.com>
2023-01-19 20:20:22 -0500

    added update and delete routes

--- src/actions.js
+++ src/actions.js
@@ -10,7 +10,7 @@ export const createAction = async ({ request }) => {
         url: formData.get("url"),
     };
 
-    await fetch(URL + "/bookMark", {
+    await fetch(URL + "/bookmark", {
         method: "post",
         headers: {
             "Content-Type": "application/json",
@@ -20,3 +20,36 @@ export const createAction = async ({ request }) => {
 
     return redirect("/");
 };
+
+export const updateAction = async({request, params}) =>{ 
+   
+    console.log("hello from update action")
+    console.log(request)
+    console.log(params.id)
+
+    // get data from form
+    const formData = await request.formData()
+
+    const updatedBookmark = {
+        title: formData.get("title"),
+        url: formData.get("url")
+    }
+    console.log("Updated bookmark is "+updatedBookmark.title+updatedBookmark.url)
+
+    await fetch(URL+"/bookmark/"+params.id, {
+        method: "put",
+        headers: { "Content-Type":"application/json" },
+        body: JSON.stringify(updatedBookmark)
+    })
+    
+    return redirect("/")
+
+}
+
+export const deleteAction = async ({params}) =>{
+    await fetch(URL+'/bookmark/'+params.id, {
+        method: "delete"
+    })
+    return redirect("/")
+
+}
