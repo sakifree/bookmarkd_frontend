682d5ba893c7d1409d6ee07ea045d0f13b7739fb
Naveen Muralidharan <naveenspec@gmail.com>
2023-01-21 09:31:09 -0500

    updated styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom"
 
 function Header(props) {
   return (
+    <div className="container">
     <nav className="nav">
       <Link to="/">
         <div>Book Mark App</div>
       </Link>
     </nav>
+    <br></br>
+    </div>
+    
   )
 }
 
